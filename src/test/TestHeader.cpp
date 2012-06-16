5275ddd1acaefac177926867623cacb13d196605
Raúl Expósito <raul@raulexposito.com>
2012-06-16 14:34:10 +0200

    Se emulan algunos mensajes recibido del laser para hacer pruebas

--- src/test/TestHeader.cpp
+++ src/test/TestHeader.cpp
@@ -5,7 +5,9 @@
 #include "../header/hdr/ScanDataHeader.h"
 #include "../header/factory/hdr/HeaderFactory.h"
 #include "../body/hdr/GetParameterRequestBody.h"
+#include "../body/factory/hdr/BodyFactory.h"
 #include "../message/hdr/ResetMessage.h"
+#include "../message/hdr/Message.h"
 #include "../message/hdr/GetParameterRequestMessage.h"
 #include "../message/hdr/GetStatusRequestMessage.h"
 #include "../message/hdr/SetParameterRequestMessage.h"
@@ -52,16 +54,58 @@ void testLeerLaser() {
 
 void testGetParameter() {
 
-	Message *request = new GetParameterRequestMessage(TCP_PORT);
-	NetworkClient::getInstance()->send(request);
-	Message * response = NetworkClient::getInstance()->receive();
+//	Message *request = new GetParameterRequestMessage(TCP_PORT);
+//	NetworkClient::getInstance()->send(request);
+//	Message * response = NetworkClient::getInstance()->receive();
 
-	request->showInfo();
-	response->showInfo();
+//	request->showInfo();
+//	response->showInfo();
+	uint8_t* headerContent = new uint8_t[24];
+	headerContent[0] = 0xAF; // magic word
+	headerContent[1] = 0xFE;
+	headerContent[2] = 0xC0;
+	headerContent[3] = 0xC2;
+	headerContent[4] = 0x00; // Size of previous message, here just left to nullL
+	headerContent[5] = 0x00;
+	headerContent[6] = 0x00;
+	headerContent[7] = 0x00;
+	headerContent[8] = 0x00; // Size of data block
+	headerContent[9] = 0x00;
+	headerContent[10] = 0x00;
+	headerContent[11] = 0x08;
+	headerContent[12] = 0x00;	// Reserved + source Id
+	headerContent[13] = 0x00;
+	headerContent[14] = 0x20;	// Data Type - 2010 = command
+	headerContent[15] = 0x20;
+	headerContent[16] = 0x00;	// 4* ntpp time (s) + 4* fractions of a second
+	headerContent[17] = 0x05;
+	headerContent[18] = 0x26;
+	headerContent[19] = 0xBA;
+	headerContent[20] = 0x8B;
+	headerContent[21] = 0x6E;
+	headerContent[22] = 0x27;
+	headerContent[23] = 0xFC;
 
-	delete request;
-	delete response;
+	uint8_t* bodyContent = new uint8_t[8];
+	bodyContent[0] = 0x11;	// Command Type - 0020 = start measure
+	bodyContent[1] = 0x00;
+	bodyContent[2] = 0x00;
+	bodyContent[3] = 0x10;
+	bodyContent[4] = 0xDD;
+	bodyContent[5] = 0x96;
+	bodyContent[6] = 0x75;
+	bodyContent[7] = 0xA3;
+
+	Header * header = HeaderFactory::getInstance()->generateHeader(headerContent);
+	Body * body = BodyFactory::getInstance()->generateBody(header->getBodySize(), bodyContent);
+	Message * message = new Message(header, body);
+
+	message->showInfo();
+
+	delete message;
 }
+
+
 
 int main () {
 //	testStartMensajes();
